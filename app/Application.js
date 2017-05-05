74a55c09551f3bb82cb4d98bc91d29d235df92ee
OlgaIvantsova <sigyn9000@gmail.com>
2017-05-05 10:10:05 -0500

    removed store/Personnel.js

--- app/Application.js
+++ app/Application.js
@@ -2,7 +2,6 @@ Ext.define('Extjs.learning.Application', {
     extend: 'Ext.app.Application',
     name: 'Extjs.learning',
     requires: [
-        'Extjs.learning.store.Personnel',
         'Ext.tab.Panel',
         'Extjs.learning.validation.Numeric',
         'Extjs.learning.view.main.MainModel',
